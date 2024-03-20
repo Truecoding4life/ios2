3f526542ad09a85742a8e637950f79a0ad1fcbb0
Master <nghiemxthai@gmail.com>
2024-03-19 20:42:03 -0700

    added logo and make card headher bigger

--- public/js/timeStamp.js
+++ public/js/timeStamp.js
@@ -9,11 +9,14 @@ timeDisplay.html(time);
 function fixTimestamp() {
     const timestamps = $('.timestamp');
     const hourDisplay = $('.timestamp2');
+    
     for (let i = 0; i < timestamps.length; i++) {
-        let text = timestamps[i].innerHTML;
-        let date = text.slice(0, 16);
-        let time = text.slice(17,22)
-        timestamps[i].innerHTML = "Posted on " + date + " at " + time ;
+        let text = timestamps[i].innerHTML.trim();
+        let textArray = text.split(' ');
+        let date = textArray[0]+" "+textArray[1]+" "+textArray[2] + " " + textArray[3];
+        let time = textArray[4];
+        timestamps[i].innerHTML = `Posted on ${ date} at ${time.slice(0,5)}` ;
     }
 }
+
 fixTimestamp();

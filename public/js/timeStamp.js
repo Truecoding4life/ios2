67da9ba59a90b3ac92da339cdfe85bbca129a932
Master <nghiemxthai@gmail.com>
2024-03-19 20:58:46 -0700

    make date display more accurate and time to be AM and PM

--- public/js/timeStamp.js
+++ public/js/timeStamp.js
@@ -6,6 +6,8 @@ const time = dateDisplay.text().slice(17,26)
 dateDisplay.html(` ${date}`);
 timeDisplay.html(time);
 
+
+// This will target every timestamp class text and return it with the correct time to display
 function fixTimestamp() {
     const timestamps = $('.timestamp');
     const hourDisplay = $('.timestamp2');
@@ -15,8 +17,27 @@ function fixTimestamp() {
         let textArray = text.split(' ');
         let date = textArray[0]+" "+textArray[1]+" "+textArray[2] + " " + textArray[3];
         let time = textArray[4];
-        timestamps[i].innerHTML = `Posted on ${ date} at ${time.slice(0,5)}` ;
+        
+        timestamps[i].innerHTML = `Posted on ${ date} at ${fixTimeDisplay(time)}` ;
     }
 }
 
 fixTimestamp();
+
+
+// This will take in the data string 12:32:00 and return 12:32 PM
+function fixTimeDisplay(date){
+    let dateArray = date.split(':');
+    let am = "AM";
+    let pm = "PM";
+    let hour = dateArray[0];
+    let minutes = dateArray[1]
+    result = `${hour}:${minutes} ${am}`;
+    let checkNum = parseInt(hour);
+    if(hour > 11){
+        hour = hour - 12;
+        result = `${hour}:${minutes} ${pm}`;
+        return result;
+    }
+    return result;
+};
